c7a98cce53c6933d5447f53ba24c1debd85a96c5
remakeaccess22 <alayacyacjude@gmail.com>
2025-08-19 12:25:46 +0800

    Props and Emit

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,18 +1,15 @@
 import AssignmentList from "./AssignmentList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
 export default {
-  components: { AssignmentList },
+  components: { AssignmentList, AssignmentCreate },
 
   template: `
   <section class="space-y-6">
     <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
     <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
-    <form @submit.prevent="add">
-    <div class="border border-gray-600 text-black">
-      <input v-model="newAssignment" placeholder="New Assignment..." class="p-2"/>
-      <button type="submit" class="bg-white p-2 border-l">Add</button>
-    </div>
-    </form>
+    <assignment-create @add="add"></assignment-create>
   </section>
   `,
 
@@ -23,8 +20,6 @@ export default {
         { name: "Review Code", completed: false, id: 2 },
         { name: "Write Tests", completed: false, id: 3 },
       ],
-
-      newAssignment: [],
     };
   },
 
@@ -38,13 +33,12 @@ export default {
   },
 
   methods: {
-    add() {
+    add(name) {
       this.assignments.push({
-        name: this.newAssignment,
+        name: name,
         completed: false,
         id: this.assignments.length + 1,
       });
-      this.newAssignment = "";
     },
   },
 };
